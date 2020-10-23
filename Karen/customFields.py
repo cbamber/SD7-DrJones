e13ca1887ff55b00a8c8d89a8ee2adcb6e82cd77
Karen Bennett <kbennett@nanthealth.com>
2020-10-23 09:49:30 +0100

    function to return stories by MVF ID

---
+++ Karen/customFields.py
@@ -0,0 +1,47 @@
+#  Python quickquery - run the setup and load JIRA.
+#  Generally intending to run and then drop to the interpreter via "python -i quickquery.py"
+import getpass
+import pprint
+from jira import JIRA
+#import xlsxwriter
+
+servername = "https://nhjira.nanthealth.com"
+username = 'kbennett'
+#username = 'set-me-to-a-name'
+#mvf_id = '1702'
+#workbook = xlsxwriter.Workbook('SparkDay7.xlsx')
+#worksheet = workbook.add_worksheet()
+
+def getauth ( username ):
+    print('Enter JIRA password for ' + username)
+    pw = getpass.getpass("JIRA password:")
+    return pw
+
+def getmvfstories ( mvf_id ):
+    stories = jira.search_issues('project= NNO OR project= ESR AND issuetype in (Bug,Story,Task) AND \'MVF ID\' =' + mvf_id, maxResults=500)
+    print(stories)
+
+
+# Don't encode passwords in Git anything :)
+password = getauth(username)
+
+jira = JIRA(server=servername, basic_auth=(username,password))
+	
+
+# ################################################
+# # Grab all the JIRIA custom field names
+# # ------------------------------------------------
+# resp=jira.fields()
+# fmap = {}
+# for i in resp:
+#     field_name=i[u'name']
+#     field_id=i[u'id']
+#     fmap[field_name]=field_id
+#     #print (i['name'])
+# pprint.pprint(fmap)
+
+getmvfstories('1702')
+
+# issue = jira.issue('MVF-1702')
+# print(issue)
+
